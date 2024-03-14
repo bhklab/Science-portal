73315cebb0ef484643f9c389df0661506dee3a51
mattbocc <matthew.boccalon@live.ca>
2024-03-14 10:21:44 -0400

    first commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(process.env.PORT || 2000);
+  console.log("The back-end is running at https://localhost:" + process.env.PORT);
+}
+bootstrap();
