22ef70dcaa7a230468ccbe2972f65b3027a1a249
Matthew Boccalon <matthew.boccalon@live.ca>
2024-06-03 12:32:00 -0400

    removing a line that uses .env before it's instantiated

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PublicationModule } from './publication/publication.module';
-console.log(process.env.MONGODB_URL);
 @Module({
   imports: [
 	MongooseModule.forRoot(process.env.MONGODB_URL),
