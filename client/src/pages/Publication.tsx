3c46df769112d9a861a4f8c9dac68c3fabec2a43
mattbocc <matthew.boccalon@live.ca>
2025-03-26 15:23:21 -0400

    Bug fix on individual publication page where scientists weren't being retrieved

--- client/src/pages/Publication.tsx
+++ client/src/pages/Publication.tsx
@@ -3,11 +3,20 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import Pub from '../interfaces/Pub';
 import PublicationModalContent from '../components/PublicationModal/PublicationModalContent';
+import Author from 'interfaces/Author';
+
+interface PublicationModalProps {
+    isVisible: boolean;
+    onHide: () => void;
+    pub: Pub | null;
+    scientists: Author[];
+}
 
 const Publication: React.FC = () => {
     const { doi } = useParams();
     const [pub, setPub] = useState<Pub | null>(null);
     const [editMode, setEditMode] = useState<boolean>(false);
+    const [scientists, setScientists] = useState<Author[]>([]);
 
     useEffect(() => {
         //Fetch publication based on DOI in the URL
@@ -25,9 +34,34 @@ const Publication: React.FC = () => {
         getPublication();
     }, [doi]);
 
+    // Fetch authors on load
+    useEffect(() => {
+        const getScientists = async () => {
+            try {
+                const res = await axios.get(`/api/authors/all`);
+                setScientists(
+                    res.data.map((scientist: Author) => ({
+                        ...scientist,
+                        fullName: `${scientist.lastName}, ${scientist.firstName}`
+                    }))
+                );
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        getScientists();
+    }, []);
+
     return (
         <div className="pt-28 md:px-0 px-[120px] bg-white min-h-screen">
-            {pub && <PublicationModalContent pub={pub} editMode={editMode} setEditMode={setEditMode} />}
+            {pub && (
+                <PublicationModalContent
+                    pub={pub}
+                    editMode={editMode}
+                    setEditMode={setEditMode}
+                    scientists={scientists}
+                />
+            )}
         </div>
     );
 };
