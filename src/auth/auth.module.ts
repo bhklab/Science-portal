6b48a2caeb97c16371c7dacfc05e656bdb471f33
mattbocc <matthew.boccalon@live.ca>
2026-03-26 14:03:05 -0400

    removing unused code

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { KeycloakConnectModule } from 'nest-keycloak-connect';
 import * as dotenv from 'dotenv'; dotenv.config();
 
 
