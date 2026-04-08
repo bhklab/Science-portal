ad196d5176c4f14f560f6d129cd6d5ea6f431a9d
mattbocc <matthew.boccalon@live.ca>
2026-04-08 15:09:17 -0400

    cleanup: refactored and cleaned up individual publication page (remove redundant and unused code)

--- client/src/pages/Publication.tsx
+++ client/src/pages/Publication.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState, useContext, useRef } from 'react';
-import { useParams, Link, useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { Toast } from 'primereact/toast';
 import axios from 'axios';
-import Pub, { createDefaultPub } from '../interfaces/Pub';
+import Pub from '../interfaces/Pub';
 import PublicationModalContent from '../components/Modals/PublicationModalContent';
 import Author from 'interfaces/Author';
 import { AuthContext } from 'hooks/AuthContext';
@@ -12,12 +12,10 @@ const Publication: React.FC = () => {
     const [pub, setPub] = useState<Pub | null>(null);
     const [editMode, setEditMode] = useState<boolean>(false);
     const [scientists, setScientists] = useState<Author[]>([]);
-    // const [fanoutEmail, setFanoutEmail] = useState<string>('');
 
     const toast = useRef<Toast>(null);
 
     const authContext = useContext(AuthContext);
-    // const location = useLocation();
 
     useEffect(() => {
         //Fetch publication based on DOI in the URL
@@ -32,16 +30,7 @@ const Publication: React.FC = () => {
                 }
             }
         };
-        // const getFanoutEmail = async () => {
-        //     try {
-        //         const res = await axios.get('/api/emails/fanout');
-        //         setFanoutEmail(res.data);
-        //     } catch (error) {
-        //         console.log(error);
-        //     }
-        // };
         getPublication();
-        // getFanoutEmail();
     }, [doi]);
 
     // Fetch authors on load
@@ -62,70 +51,12 @@ const Publication: React.FC = () => {
         getScientists();
     }, []);
 
-    // const fanoutApproval = async (verdict: boolean) => {
-    //     //Ensure the publication isn't null before assignment
-    //     if (pub) {
-    //         setPub({ ...pub, fanout: { request: true, completed: true, verdict: true } });
-    //     }
-    //     const response = await axios.post('/api/emails/fanout/send', { pub: pub, verdict: verdict });
-    //     const data = response.data;
-
-    //     if (data.status === 200) {
-    //         toast.current?.show({
-    //             severity: 'success',
-    //             summary: 'Successful Email Fanout Approval',
-    //             detail: data.message,
-    //             life: 8000
-    //         });
-    //     } else {
-    //         toast.current?.show({
-    //             severity: 'error',
-    //             summary: 'Email Fanout Approval Error',
-    //             detail: data.message,
-    //             life: 8000
-    //         });
-    //     }
-    // };
-
     return (
         <div className="bg-white">
-            {/* {pub?.fanout?.request && !pub?.fanout?.completed && !authContext?.user && (
-                <div className="flex justify-center items-center gap-2 sticky top-16 w-full py-5 bg-gray-100 border-b-1 ">
-                    <h2 className="text-bodyXl font-semibold">
-                        To approve this publication as an admin, please login{' '}
-                        <Link to="/login" state={{ from: location }} className="text-blue-700 font-semibold">
-                            here
-                        </Link>
-                    </h2>
-                </div>
-            )}
-            {pub?.fanout?.request && !pub?.fanout?.completed && fanoutEmail.includes(authContext?.user?.email) && (
-                <div className="flex flex-col justify-center items-center gap-2 sticky top-16 w-full py-3 bg-gray-100 border-b-1 ">
-                    <h2 className="text-bodyXl font-semibold">
-                        Would you like to approve the publication fanout request?
-                    </h2>
-                    <div className="flex flex-row gap-4">
-                        <button
-                            className="text-green-600 text-bodyMd font-semibold transition ease-in-out hover:scale-105"
-                            onClick={() => fanoutApproval(true)}
-                        >
-                            Approve
-                        </button>
-                        <button
-                            className="text-red-600 text-bodyMd font-semibold transition ease-in-out hover:scale-105"
-                            onClick={() => fanoutApproval(false)}
-                        >
-                            Reject
-                        </button>
-                    </div>
-                </div>
-            )} */}
-
-            <div className="pt-28 md:px-0 px-[120px] bg-white min-h-screen">
+            <div className="pt-28 mmd:px-[10px] px-[120px] bg-white min-h-screen">
                 {pub && (
                     <PublicationModalContent
                         pub={pub}
-                        setPub={setPub}
                         editMode={editMode}
                         setEditMode={setEditMode}
                         scientists={scientists}
